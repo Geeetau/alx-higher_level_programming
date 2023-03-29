543e657d5a28711fd598c1ac711cf89125521675
Joseph <josephgitau6703@gmail.com>
2023-03-29 13:17:45 -0700

    task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Square class definition"""
+
+class Square:
+    """Represent a square
+    Attributes:
+    __size (int): size of a side of the square
+    """
+    def __init__(self, size):
+        """Initialize a square
+        Args:
+        size (int): size of a side of tje square
+        Returns: None
+        """
+        self.__size = size
