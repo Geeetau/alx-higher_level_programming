54a1e8255034357fb0fbc6b46796609afedfbd6c
Joseph <josephgitau6703@gmail.com>
2023-04-08 08:44:58 -0700

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""supplies one function, say_my_name"""
+"""
+This is the "3-say_my-name" module.
+The 3-say_my_name  module supplies one function, say_my_name.
+"""
 
 
 def say_my_name(first_name, last_name=""):
