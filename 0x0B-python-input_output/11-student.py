9214891f9a7b4511bba49a0613e3ade92035973f
Joseph <josephgitau6703@gmail.com>
2023-04-13 16:56:28 -0700

    task 11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -6,6 +6,8 @@ contains the student function
 
 class Student:
     """representation of a student"""
+    def __init__(self, first_name, last_name, age):
+        """Initializes the student"""
     self.first_name = first_name
     self.last_name = last_name
     self.age = age
