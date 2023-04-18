4fd82bf55b592792fde89cd8c173c12fd06d502a
Joseph <josephgitau6703@gmail.com>
2023-04-18 11:25:36 -0700

    task 12

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -11,3 +11,8 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square."""
         super().__init__(size, size, x, y, id)
+
+     @property
+    def size(self):
+        """Get/set the size of the Square."""
+        return self.width
