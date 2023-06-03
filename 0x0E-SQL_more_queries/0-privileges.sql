05f7c4437808be54d8bbaaf169f84302a29b8fba
Joseph <josephgitau6703@gmail.com>
2023-06-03 15:13:01 -0700

    task 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- lists all priviledges of the users user_0d_1 and user_0d_2.
-SHOW GRANTS FOR `user_0d_1`@`localhost`;
-SHW GRANTS FOR `user_0d_2`@`localhost`;
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHW GRANTS FOR 'user_0d_2'@'localhost';
