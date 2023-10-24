48926492f36e17a9372c6b44097eb8a5446969fc
Joseph Gitau <josephgitau6703@gmail.com>
2023-10-24 23:15:22 +0300

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""
-contains the class_to_json function
-"""
-
-
-def class_to_json(obj):
-    """ returns the dictionary description with simple data structure"""
-    return obj.__dict__
